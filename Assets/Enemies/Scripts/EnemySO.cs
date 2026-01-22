0c18e2255468ed8595e6b1befccbcf655a97b87f
ParallaxDQ <49080289+ParallaxDQ@users.noreply.github.com>
2026-01-22 15:23:27 +0000

    Update 1
    Stuff done

---
+++ Assets/Enemies/Scripts/EnemySO.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Game/Enemy")]
+public class EnemySO : ScriptableObject
+{
+    [Header("Basic Info")]
+    [Tooltip("Name displayed in combat")]
+    public string enemyName = "Enemy";
+    [Tooltip("EnemyID")]
+    public int enemyID;
+
+
+    [Header("Stats")]
+    [Tooltip("Max enemy health")]
+    public int maxHealth;
+    [Tooltip("Enemy damage to player")]
+    public int Damage;
+    [Tooltip("Enemy speed")]
+    public int Speed;
+    [Tooltip("Levels of XP to drop (1, 2 or 3)")]
+    public int xpDrop;
+
+    [Header("Visual")]
+    public Sprite enemySprite;
+    [Tooltip("Animator with this enemy's animations")]
+    public AnimatorOverrideController animatorController;
+    public float displayScale = 5f;
+
+    /// <summary>
+    /// Convert this ScriptableObject to runtime EnemyData
+    /// </summary>
+    public EnemyScript ToEnemyData()
+    {
+        return new EnemyScript
+        {
+            enemyName = this.enemyName,
+            enemyID = this.enemyID,
+            enemySprite = this.enemySprite,
+            enemyMaxHealth = this.maxHealth,
+            enemyDamage = this.Damage,
+            enemySpeed = this.Speed,
+            enemyCurrentHealth = this.maxHealth, // Start at full HP
+            xpDrop = this.xpDrop,
+        };
+    }
+}
+
+
---
+++ Assets/Enemies/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+    private int currentHealth;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+
+    }
+}
